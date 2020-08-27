fe802e04d276f4718897157b43a50a59b7f4b8bc
Arindam Deb <Arindamdeb2000@icloud.com>
2020-08-27 13:11:53 +0530

    Update Autoresponse.js

--- Autoresponse.js
+++ Autoresponse.js
@@ -41,7 +41,7 @@ let RecruiterEmail = function(recipient, sender) {
 
     let negativeResponse = `No ${sender.firstName}, I want none of these things and want to be stagnant with my same old job and to be phased out of new opportunities.`;
 
-    let valueProposition = 'Staying on top of your industry is necessary. Partner with an actual IT guy not a recruiter that just cares about money and shoving you through a square hole.';
+    let valueProposition = 'Staying on top of your industry is necessary. Partner with the recruiter to understand more about the role. You never know,this might be the job you are looking forward to.';
 
     function init() {
         elements.salutation.innerHTML = `${recipient},`;
